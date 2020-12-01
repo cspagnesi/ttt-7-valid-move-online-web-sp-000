efc81460a1e35cfaa1fcd1cb2608ee177bf21573
Christopher Spagnesi <christopherspagnesi@gmail.com>
2020-12-01 22:35:26 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -19,7 +19,7 @@ end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 # ./lib/valid_move.rb
-returns true for a valid position on an empty board 
-returns true for a valid position on a non-empty board 
-returns nil or false for an occupied position 
-returns nil or false for a position that is not on the board 
+# returns true for a valid position on an empty board 
+# returns true for a valid position on a non-empty board 
+# returns nil or false for an occupied position 
+# returns nil or false for a position that is not on the board 
