493904a8b3bfcd61d22fd62ddce97619b2defaad
Christopher Spagnesi <christopherspagnesi@gmail.com>
2020-12-01 22:29:05 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,12 +1,12 @@
 def valid_move?
   
-  if position_taken?
+  if position_taken?(board, index)
   
-  # if board[index] == == "X" || board[index] == "O"
-  # return TRUE
+  if board[index] == == "X" || board[index] == "O"
+  return TRUE
     
-  # elsif index.between?(0, 9)
-  # return TRUE
+  elsif index.between?(0, 9)
+  return TRUE
   
   else
   return false
